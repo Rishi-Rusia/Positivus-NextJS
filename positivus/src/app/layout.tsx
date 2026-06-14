b51e5e098a35e7a56fc6f4dc96899ed2f0d9d055
Rishi Rusia <rishirusia1@gmail.com>
2026-06-14 12:09:55 +0530

    refactored layout.tsx

--- positivus/src/app/layout.tsx
+++ positivus/src/app/layout.tsx
@@ -1,34 +1,3 @@
-// import type { Metadata } from "next";
-// import { Geist } from "next/font/google";
-// import Navbar from "../components/layout/Navbar"
-// import "./globals.css";
-
-// const geist = Geist({
-// subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {
-// title: "Positivus",
-// description: "Digital marketing agency website",
-// };
-
-// export default function RootLayout({
-// children,
-// }: Readonly<{
-// children: React.ReactNode;
-// }>) {
-// return (<html lang="en"> 
-// <head>
-//     <link rel="preconnect" href="https://fonts.googleapis.com" />
-//     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-//     <link
-//       href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
-//       rel="stylesheet"
-//     />
-// </head>
-// <body className={geist.className}><Navbar />{children}</body> </html>);
-// }
-
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
